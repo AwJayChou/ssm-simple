ac5a9450c06790a0e3a026d9485fa077d052c13d
jaychou <742949962@qq.com>
2020-08-09 11:00:04 +0800

    feat: init projecct

---
+++ src/main/java/com/jay/ssm/mapper/ItemMapper.java
@@ -0,0 +1,31 @@
+package com.jay.ssm.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+import com.jay.ssm.po.Item;
+import com.jay.ssm.po.ItemExample;
+
+public interface ItemMapper {
+    int countByExample(ItemExample example);
+
+    int deleteByExample(ItemExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Item record);
+
+    int insertSelective(Item record);
+
+    List<Item> selectByExample(ItemExample example);
+
+    Item selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Item record, @Param("example") ItemExample example);
+
+    int updateByExample(@Param("record") Item record, @Param("example") ItemExample example);
+
+    int updateByPrimaryKeySelective(Item record);
+
+    int updateByPrimaryKey(Item record);
+}
---
+++ src/main/java/com/jay/ssm/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.jay.ssm.service;
+
+import java.util.List;
+
+import com.jay.ssm.po.Item;
+
+public interface ItemService {
+
+	List<Item> queryItemList();
+}
